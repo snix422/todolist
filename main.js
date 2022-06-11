b9c0b0067a9b0e21a3c6c8e34e4ccf1c707294d6
Mariusz Wójcik <mariusz.wojcik32@onet.pl>
2022-06-11 15:49:51 +0200

    vowelcounter

---
+++ main.js
@@ -0,0 +1,61 @@
+const vowelContainer = document.querySelector('.vowel-container');
+const btnCountVowel = document.querySelector('.btnCount');
+const input = document.querySelector('.contentWord')
+
+
+const countVowel = () => {
+    const divVowel = document.createElement('div');
+    const divVowelItem = document.createElement('div');
+    const word = document.createElement('span');
+    const counterVowel = document.createElement('span');
+
+    vowelContainer.appendChild(divVowel);
+    divVowel.appendChild(divVowelItem);
+    divVowelItem.appendChild(word);
+    divVowelItem.appendChild(counterVowel);
+
+
+    divVowel.classList.add('vowel-score');
+    divVowelItem.classList.add('vowel-score_item');
+
+    let str = input.value;
+
+    const arr = str.split('');
+    console.log(arr);
+
+    let sum = 0;
+    word.textContent = input.value;
+
+    for (i = 0; i < arr.length; i++) {
+
+        if (arr[i].match('a')) {
+            sum = sum + 1;
+        }
+        if (arr[i].match('e')) {
+            sum = sum + 1;
+        }
+        if (arr[i].match('i')) {
+            sum = sum + 1;
+        }
+        if (arr[i].match('o')) {
+            sum = sum + 1;
+        }
+        if (arr[i].match('u')) {
+            sum = sum + 1;
+        }
+        if (arr[i].match('y')) {
+            sum = sum + 1;
+        }
+
+        console.log(arr[i]);
+
+    }
+    counterVowel.textContent = `Liczba samogłosek: ${sum}`;
+
+    input.value = "";
+
+
+
+}
+
+btnCountVowel.addEventListener('click', countVowel);
